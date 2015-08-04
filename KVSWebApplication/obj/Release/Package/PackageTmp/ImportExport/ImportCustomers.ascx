a4d1e85cfcca52eee44574830fce99322b27a338
Yury Spiridonov <spiridonov.yury@gmail.com>
2015-08-04 13:05:06 +0200

    delete release files

--- KVSWebApplication/obj/Release/Package/PackageTmp/ImportExport/ImportCustomers.ascx
+++
@@ -1,1 +0,0 @@
-﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="ImportCustomers.ascx.cs" Inherits="KVSWebApplication.ImportExport.ImportCustomers" %>
--- KVSWebApplication/obj/Release/Package/PackageTmp/Scripts/scripts.js
+++
@@ -1,8 +0,0 @@
-﻿function openRadWindowZulPos() {
-    $find("MainContentPlaceHolder_ZulassungNachbearbeitunguserControl_RadWindowZul_Product").show();
-}
-function openRadWindowZulNeuzulassungPos() {
-    $find("MainContentPlaceHolder_ZulassungsstelleuserControl_RadWindowZul_Product").show();
-}
-
-  
