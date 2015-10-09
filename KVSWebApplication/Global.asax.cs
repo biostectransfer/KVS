202cc5724aa74a0db6227be4bba984720de6fb3b
Yury Spiridonov <spiridonov.yury@gmail.com>
2015-10-09 11:58:00 +0200

    Default culture

--- KVSWebApplication/Global.asax.cs
+++ KVSWebApplication/Global.asax.cs
@@ -2,7 +2,9 @@
 using Microsoft.Practices.Unity;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Http;
 using Unity.WebApi;
@@ -18,6 +20,12 @@ namespace KVSWebApplication
             GlobalConfiguration.Configuration.DependencyResolver = new UnityDependencyResolver(container);
 
             Spire.Barcode.BarcodeSettings.ApplyKey("SB4UT-1SE20-3T96D-50CTF-K878A");
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("de-DE");
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("de-DE");
         }
     }
 }
